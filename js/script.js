8b12ed2acfa539788c606efbb221b5ea18501637
Inna Didukh <infarmation@gmail.com>
2019-05-18 20:32:34 +0300

    created header by using simantic have not finish styling else

---
+++ js/script.js
@@ -0,0 +1,15 @@
+let modal = document.getElementById('modal-form');
+var btn = document.getElementById('btn');
+var btnClose = document.getElementById("close-form-modal");
+
+// btn.onclick = function () {
+// 	modal.style.display = "block";
+// };
+
+// function viewForm(){
+//   document.getElementById("madal-form").style.display = "block";
+// };
+
+// function closeForm() {
+// 	document.getElementById("madal-form").style.display = "none"
+// }
