166b990cbe5f7b796040ac7f792d47c1fa482b40
Inna Didukh <infarmation@gmail.com>
2019-05-18 20:46:27 +0300

    script: add function for madal-form line 5

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ let modal = document.getElementById('modal-form');
 var btn = document.getElementById('btn');
 var btnClose = document.getElementById("close-form-modal");
 
-// btn.onclick = function () {
-// 	modal.style.display = "block";
-// };
+btn.onclick = function () {
+	modal.style.display = "block";
+};
 
 // function viewForm(){
 //   document.getElementById("madal-form").style.display = "block";
