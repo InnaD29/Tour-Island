dceea87436a370f5e51e96d4e9cd37e44732da2c
Inna Didukh <infarmation@gmail.com>
2019-05-18 22:11:11 +0300

    html-code [header is it processing]

---
+++ script/script.js
@@ -0,0 +1,7 @@
+function viewForm(){
+  document.getElementById("madal-form").style.display = "block";
+};
+
+function closeForm() {
+	document.getElementById("madal-form").style.display = "none"
+}
