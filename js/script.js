ad21f9747a8b0e5a85feca84e0091f24ffad61ae
Inna Didukh <infarmation@gmail.com>
2019-05-30 11:57:14 +0300

    finished project

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-// let modal = document.getElementById('modal-form');
-// var btn = document.getElementById('btn');
-// var btnClose = document.getElementById("close-form-modal");
 
-// btn.onclick = function () {
-// 	modal.style.display = "block";
 
-// function viewForm(){
-//   document.getElementById("modal-form").style.display = "block";
-// };
 
-// function closeForm() {
-// 	document.getElementById("modal-form").style.display = "none"
-// }
